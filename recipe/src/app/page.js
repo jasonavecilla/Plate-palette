b7e17c9b1a02686e461c52d7e1f960623e228b82
jess <120062852+jessabc@users.noreply.github.com>
2023-10-10 11:44:50 +1100

    add preline

--- recipe/src/app/page.js
+++ recipe/src/app/page.js
@@ -1,14 +1,21 @@
+'use client'
+import { useEffect } from 'react'
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import FAQAccordian from "./components/FAQAccordian"
 
 export default function Home() {
+  useEffect(() => {
+    import('preline')
+  }, [])
   return (
     <>
     <Navbar />
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         Recipe
       </main>
+      <FAQAccordian/>
       <Footer />
     </>
   );
