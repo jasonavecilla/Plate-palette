7b1a4dfe13d12f52bdad937426f38a0341f34e2f
jess <120062852+jessabc@users.noreply.github.com>
2023-10-10 13:25:32 +1100

    remove blog component

--- recipe/src/app/page.js
+++ recipe/src/app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Blog from './components/Blog'
 
 export default function Home() {
   return (
@@ -10,7 +9,6 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         Recipe
       </main>
-      <Blog/>
       <Footer />
     </>
   );
