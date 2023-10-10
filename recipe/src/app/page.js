9cbc68fe9af36083c9ec1ad0f10f33b6df31e0fd
jess <120062852+jessabc@users.noreply.github.com>
2023-10-10 12:22:31 +1100

    add blog component

--- recipe/src/app/page.js
+++ recipe/src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Blog from './components/Blog'
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         Recipe
       </main>
+      <Blog/>
       <Footer />
     </>
   );
