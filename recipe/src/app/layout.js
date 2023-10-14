704da765c1e5786bf2dfa93b6a7148d5bb7015aa
unknown <johncarlolipa@gmail.com>
2023-10-14 12:19:35 +0800

    fix layout

--- recipe/src/app/layout.js
+++ recipe/src/app/layout.js
@@ -1,3 +1,5 @@
+import Footer from "./components/Footer";
+import NavBar from "./components/Navbar";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,7 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head></head>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
