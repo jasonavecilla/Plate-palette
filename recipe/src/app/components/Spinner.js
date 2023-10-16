e9de26ff888426bf5da54370faeded547dece93e
jess <120062852+jessabc@users.noreply.github.com>
2023-10-16 12:30:24 +1100

    add spinner component

---
+++ recipe/src/app/components/Spinner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Spinner() {
+  return (
+    <div className='flex justify-center items-center'>
+        <div className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-olive rounded-full " role="status" aria-label="loading">
+            <span className="sr-only">Loading...</span>
+        </div>
+    </div>
+  )
+}
+
+export default Spinner
