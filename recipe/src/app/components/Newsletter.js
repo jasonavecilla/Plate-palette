00211f1577b7cd777959f3b85dcd991930a40e00
unknown <johncarlolipa@gmail.com>
2023-10-16 10:02:13 +0800

    fix newslter layout error

--- recipe/src/app/components/Newsletter.js
+++ recipe/src/app/components/Newsletter.js
@@ -1,53 +1,39 @@
 export default function Newsletter() {
   return (
-    <div className="max-w-full mx-auto py-16 sm:py-24">
-      <div className="mx-auto sm:px-6 lg:px-8">
-
-        <div
-          className="max-w-full mx-auto py-16 sm:py-24"
-          style={{
-            backgroundImage:
-              "url(https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)",
-            backgroundSize: "cover",
-          }}
-        >
-          <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
-            Stay Updated with Our Latest Recipe.
-          </h2>
-          <p className="mx-auto mt-4 max-w-xl text-center text-lg leading-8 text-white font-semibold">
-
-        <div className=" bg-olive-300 px-6 py-24 sm:px-24 xl:py-32">
-          <h2 className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-dark sm:text-4xl">
-            Stay Updated with Our Latest Recipe.
-          </h2>
-          <p className="mx-auto mt-4 max-w-xl text-center text-lg leading-8 text-dark-400 font-semibold">
-
-            Join our newsletter to stay updated with our latest and most
-            delicious recipes, culinary tips, and mouthwatering meal ideas
-            delivered straight to your inbox!
-          </p>
-          <form className="mx-auto mt-10 flex max-w-md gap-x-4">
-            <label htmlFor="email-address" className="sr-only">
-              Email address
-            </label>
-            <input
-              id="email-address"
-              name="email"
-              type="email"
-              autoComplete="email"
-              required
-              className="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
-              placeholder="Enter your email"
-            />
-            <button
-              type="submit"
-              className="flex-none rounded-md bg-yellowgreen px-3.5 py-2.5 text-sm font-semibold text-dark-700 shadow-sm hover:bg-yellowgreen-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-            >
-              Subscribe
-            </button>
-          </form>
-        </div>
+    <div
+      className="bg-cover bg-center py-16 sm:py-24"
+      style={{
+        backgroundImage: `url(https://images.unsplash.com/photo-1496412705862-e0088f16f791?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80)`,
+      }}
+    >
+      <div className="mx-auto max-w-2xl text-center text-3xl font-bold tracking-tight text-white sm:text-4xl">
+        Stay Updated with Our Latest Recipe.
       </div>
+      <p className="mx-auto mt-4 max-w-xl text-center text-lg leading-8 text-white font-semibold">
+        Join our newsletter to stay updated with our latest and most delicious
+        recipes, culinary tips, and mouthwatering meal ideas delivered straight
+        to your inbox!
+      </p>
+      <form className="mx-auto mt-10 flex max-w-md gap-x-4">
+        <label htmlFor="email-address" className="sr-only">
+          Email address
+        </label>
+        <input
+          id="email-address"
+          name="email"
+          type="email"
+          autoComplete="email"
+          required
+          className="min-w-0 flex-auto rounded-md border-0 bg-white px-3.5 py-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-white sm:text-sm sm:leading-6"
+          placeholder="Enter your email"
+        />
+        <button
+          type="submit"
+          className="flex-none rounded-md bg-yellowgreen px-3.5 py-2.5 text-sm font-semibold text-dark-700 shadow-sm hover:bg-yellowgreen-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
+        >
+          Subscribe
+        </button>
+      </form>
     </div>
   );
 }
