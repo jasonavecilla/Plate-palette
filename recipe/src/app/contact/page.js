3a60c5130b1608f59af27a4df826df19f7e84229
unknown <johncarlolipa@gmail.com>
2023-10-22 10:03:46 +0800

    edited about page

--- recipe/src/app/contact/page.js
+++ recipe/src/app/contact/page.js
@@ -1,131 +1,14 @@
-export default function Contact() {
-  return (
-    <div className="relative isolate bg-white">
-      <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div className="relative px-6 pb-20 pt-24 sm:pt-32 lg:static lg:px-8 lg:py-48">
-          <div className="lg:absolute lg:inset-0 lg:right-1/2">
-            <img
-              className="h-64 w-full bg-gray-50 object-cover sm:h-80 lg:absolute lg:h-full"
-              src="https://images.unsplash.com/photo-1563865436874-9aef32095fad?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fHJlY2lwZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-            />
-          </div>
-        </div>
+import React from "react";
+import Contact from "../components/Contact";
+import Newsletter from "../components/Newsletter";
+import FAQAccordian from "../components/FAQAccordian";
 
-        <form
-          action="#"
-          method="POST"
-          className="px-6 pb-24 pt-20 sm:pb-32 lg:px-8 lg:py-4"
-        >
-          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
-            <div className="mb-10">
-              <h2 className="text-3xl font-bold tracking-tight text-dark-700">
-                Feel free to reach out
-              </h2>
-              <p className="mt-6 text-lg leading-8 text-dark-700 italic">
-                Do not hesitate to contact us with any questions or feedback. We
-                are here to assist you with anything you need. Your input
-                matters to us.
-              </p>
-            </div>
-            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-              <div>
-                <label
-                  htmlFor="first-name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
-                >
-                  First name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-dark-700 shadow-sm ring-1 ring-inset ring-dark-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellowgreen-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div>
-                <label
-                  htmlFor="last-name"
-                  className="block text-sm font-semibold leading-6 text-dark-700"
-                >
-                  Last name
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="text"
-                    name="last-name"
-                    id="last-name"
-                    autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-dark-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellowgreen-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="email"
-                  className="block text-sm font-semibold leading-6 text-dark-700"
-                >
-                  Email
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    autoComplete="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-dark-700 shadow-sm ring-1 ring-inset ring-dark-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellowgreen-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="phone-number"
-                  className="block text-sm font-semibold leading-6 text-dark-700"
-                >
-                  Phone number
-                </label>
-                <div className="mt-2.5">
-                  <input
-                    type="tel"
-                    name="phone-number"
-                    id="phone-number"
-                    autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-dark-700 shadow-sm ring-1 ring-inset ring-dark-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellowgreen-500 sm:text-sm sm:leading-6"
-                  />
-                </div>
-              </div>
-              <div className="sm:col-span-2">
-                <label
-                  htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-dark-700"
-                >
-                  Message
-                </label>
-                <div className="mt-2.5">
-                  <textarea
-                    name="message"
-                    id="message"
-                    rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-dark-700 shadow-sm ring-1 ring-inset ring-dark-700 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-yellowgreen-600 sm:text-sm sm:leading-6"
-                    defaultValue={""}
-                  />
-                </div>
-              </div>
-            </div>
-            <div className="mt-8 flex justify-end">
-              <button
-                type="submit"
-                className="rounded-md bg-yellowgreen px-3.5 py-2.5 text-center text-sm font-semibold text-black shadow-sm hover:bg-yellowgreen-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Send message
-              </button>
-            </div>
-          </div>
-        </form>
-      </div>
-    </div>
+export default function page() {
+  return (
+    <>
+      <Contact />
+      <Newsletter />
+      <FAQAccordian />
+    </>
   );
 }
