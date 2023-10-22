e790edb880e93f363f7e2f5e106aa0f90259dfad
unknown <johncarlolipa@gmail.com>
2023-10-22 09:17:03 +0800

    added comment headline

--- recipe/src/app/components/Comment.js
+++ recipe/src/app/components/Comment.js
@@ -32,80 +32,83 @@ const commentData = [
 
 export default function Comment() {
   return (
-    <section className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
-      <div className="max-w-2xl mx-auto px-4">
-        <div className="flex justify-between items-center mb-6">
-          <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
-            Discussion ({commentData.length})
-          </h2>
-        </div>
-        <form className="mb-6">
-          <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-            <label className="sr-only">Your comment</label>
-            <textarea
-              id="comment"
-              rows="6"
-              className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-dark focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-              placeholder="Write a comment..."
-              required
-            ></textarea>
+    <section>
+      <h1 className="text-center font-bold text-3xl">Join the Conversation: Share Your Thoughts and Opinions</h1>
+      <div className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
+        <div className="max-w-2xl mx-auto px-4">
+          <div className="flex justify-between items-center mb-6">
+            <h2 className="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
+              Discussion ({commentData.length})
+            </h2>
           </div>
-          <button
-            type="submit"
-            className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-dark bg-yellowgreen rounded-lg focus:ring-4 focus:ring-yellowgreen-300 dark:focus:ring-primary-900 hover:bg-yellowgreen-600"
-          >
-            Post comment
-          </button>
-        </form>
-        {commentData.map((comment) => (
-          <article
-            key={comment.id}
-            className="p-6 text-base bg-white rounded-lg dark:bg-gray-900"
-          >
-            <footer className="flex justify-between items-center mb-2">
-              <div className="flex items-center">
-                <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
-                  <img
-                    className="mr-2 w-7 h-7 rounded-full"
-                    src={comment.imgUrls}
-                    alt={comment.name}
-                  />
-                  {comment.name}
-                </p>
-                <p className="text-sm text-gray-600 dark:text-gray-400">
-                  {comment.dateTime}
-                </p>
-              </div>
-            </footer>
-            <p className="text-gray-500 dark:text-gray-400">
-              {comment.content}
-            </p>
-
-            <div className="flex items-center mt-4 space-x-4">
-              <button
-                type="button"
-                className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
-              >
-                <svg
-                  className="mr-1.5 w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 20 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
-                  />
-                </svg>
-                Reply
-              </button>
+          <form className="mb-6">
+            <div className="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
+              <label className="sr-only">Your comment</label>
+              <textarea
+                id="comment"
+                rows="6"
+                className="px-0 w-full text-sm text-gray-900 border-0 focus:ring-dark focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
+                placeholder="Write a comment..."
+                required
+              ></textarea>
             </div>
-          </article>
-        ))}
+            <button
+              type="submit"
+              className="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-dark bg-yellowgreen rounded-lg focus:ring-4 focus:ring-yellowgreen-300 dark:focus:ring-primary-900 hover:bg-yellowgreen-600"
+            >
+              Post comment
+            </button>
+          </form>
+          {commentData.map((comment) => (
+            <article
+              key={comment.id}
+              className="p-6 text-base bg-white rounded-lg dark:bg-gray-900"
+            >
+              <footer className="flex justify-between items-center mb-2">
+                <div className="flex items-center">
+                  <p className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white font-semibold">
+                    <img
+                      className="mr-2 w-7 h-7 rounded-full"
+                      src={comment.imgUrls}
+                      alt={comment.name}
+                    />
+                    {comment.name}
+                  </p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    {comment.dateTime}
+                  </p>
+                </div>
+              </footer>
+              <p className="text-gray-500 dark:text-gray-400">
+                {comment.content}
+              </p>
+
+              <div className="flex items-center mt-4 space-x-4">
+                <button
+                  type="button"
+                  className="flex items-center text-sm text-gray-500 hover:underline dark:text-gray-400 font-medium"
+                >
+                  <svg
+                    className="mr-1.5 w-3.5 h-3.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 18"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
+                    />
+                  </svg>
+                  Reply
+                </button>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     </section>
   );
