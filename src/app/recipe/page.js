98e07c1b5c8ee44affd8b5c42f93881bb061d336
ErickaConcepcion <eriorion001@gmail.com>
2023-10-24 23:21:05 +0800

    Added Recipe Image Block

--- src/app/recipe/page.js
+++ src/app/recipe/page.js
@@ -3,10 +3,12 @@ import RecipeBlock from "../components/RecipeBlock";
 import React from "react";
 import Comment from "../components/Comment";
 import SuggestedRecipe from "../components/SuggestedRecipe";
+import RecipeImage from "../components/RecipeImage";
 
 export default function page() {
   return (
     <>
+      <RecipeImage></RecipeImage>
       <RecipeBlock />
       <Comment />
       <SuggestedRecipe />
