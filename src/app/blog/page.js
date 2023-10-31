aadab4e39d47f9908e9f3830f9871e4ddc450b7e
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-10-31 14:51:30 +0800

    added newsletter

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,12 +1,16 @@
 import React from "react";
 import BlogHero from "../components/BlogHero";
 import Blog from "../components/BlogItems";
+import Newsletter from "../components/Newsletter";
 
 export default function page() {
   return (
     <section>
       <BlogHero />
       <Blog />
+      <div className='-my-10'>
+        <Newsletter />
+      </div>
     </section>
   );
 }
