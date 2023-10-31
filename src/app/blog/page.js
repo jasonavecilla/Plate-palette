62f91507a8abf251e117b182485bb2fe87f6ca68
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2023-10-31 14:41:44 +0800

    added blog items

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,10 +1,12 @@
 import React from "react";
 import BlogHero from "../components/BlogHero";
+import Blog from "../components/BlogItems";
 
 export default function page() {
   return (
     <section>
       <BlogHero />
+      <Blog />
     </section>
   );
 }
